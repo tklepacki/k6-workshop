6834956b27fc56f0d5db8d3b6ad790fa288ad870
Tomasz Klepacki <t.klepacki@wp.pl>
2024-10-08 13:39:43 +0200

    Adding Tags

--- tests/reserved.test.js
+++ tests/reserved.test.js
@@ -24,21 +24,41 @@ export function reserved_login() {
   let getCustomerAccountLoginPageRequest = {
     method: 'GET',
     url: 'https://www.reserved.com/gb/en/customer/account/login/',
+    params: {
+      tags: {
+        name: 'RE - Get Customer Account Login Page'
+      }
+    }
   }
 
   let getVarnishAjaxNewIndexRequest = {
     method: 'GET',
-    url: 'https://www.reserved.com/gb/en/varnish/ajax/newindex/?1728379786319'
+    url: 'https://www.reserved.com/gb/en/varnish/ajax/newindex/?1728379786319',
+    params: {
+      tags: {
+        name: 'RE - Get Varnish New Index'
+      }
+    }
   }
 
   let getVarnishAjaxIndexRequest = {
     method: 'GET',
-    url: 'https://www.reserved.com/gb/en/varnish/ajax/index/?1728379786327'
+    url: 'https://www.reserved.com/gb/en/varnish/ajax/index/?1728379786327',
+    params: {
+      tags: {
+        name: 'RE - Get Varnish Index'
+      }
+    }
   }
 
   let postPageInfoRequest = {
     method: 'POST',
-    url: 'https://www.reserved.com/proxydirectory/549238991762/pageInfo'
+    url: 'https://www.reserved.com/proxydirectory/549238991762/pageInfo',
+    params: {
+      tags: {
+        name: 'RE - Post Page Info'
+      }
+    }
   }
 
   function postCustomerLoginRequest(form_key) {
@@ -54,6 +74,9 @@ export function reserved_login() {
       params: {
         headers: {
           'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
+        },
+        tags: {
+          name: 'RE - Post Customer Login'
         }
       }
     }
@@ -61,30 +84,35 @@ export function reserved_login() {
 
   let getMainPageRequest = {
     method: 'GET',
-    url: 'https://www.reserved.com/gb/en/'
+    url: 'https://www.reserved.com/gb/en/',
+    params: {
+      tags: {
+        name: 'RE - Get Main Page'
+      }
+    }
   }
 
   group('Login Page - https://www.reserved.com/gb/en/customer/account/login/#login', function () {
-    let getCustomerAccountLoginPageResponse = http.get(getCustomerAccountLoginPageRequest.url)
+    let getCustomerAccountLoginPageResponse = http.get(getCustomerAccountLoginPageRequest.url, getCustomerAccountLoginPageRequest.params)
     let formKey = helper.getFormKey(getCustomerAccountLoginPageResponse)
 
-    http.get(getVarnishAjaxNewIndexRequest.url)
+    http.get(getVarnishAjaxNewIndexRequest.url, getVarnishAjaxNewIndexRequest.params)
 
-    http.get(getVarnishAjaxIndexRequest.url)
+    http.get(getVarnishAjaxIndexRequest.url, getVarnishAjaxIndexRequest.params)
 
-    http.post(postPageInfoRequest.url, null)
+    http.post(postPageInfoRequest.url, null, postPageInfoRequest.params)
 
     http.post(postCustomerLoginRequest().url, postCustomerLoginRequest(formKey).body, postCustomerLoginRequest().params)
   })
 
   group('Main Page After User Log In - https://www.reserved.com/gb/en/', function () {
-    http.get(getMainPageRequest.url)
+    http.get(getMainPageRequest.url, getMainPageRequest.params)
 
-    let response = http.get(getVarnishAjaxNewIndexRequest.url)
+    let response = http.get(getVarnishAjaxNewIndexRequest.url, getVarnishAjaxNewIndexRequest.params)
     console.log(response.body)
 
-    http.get(getVarnishAjaxIndexRequest.url)
+    http.get(getVarnishAjaxIndexRequest.url, getVarnishAjaxIndexRequest.params)
 
-    http.post(postPageInfoRequest.url, null)
+    http.post(postPageInfoRequest.url, null, postPageInfoRequest.params)
   })
 }
