d8df8ddc62e45e3652f461edca70047f79204336
Tomasz Klepacki <t.klepacki@wp.pl>
2024-10-22 13:47:04 +0200

    Adding Checks

--- tests/reserved.test.js
+++ tests/reserved.test.js
@@ -1,108 +1,132 @@
-import { group } from 'k6'
+import { group, check } from 'k6'
 import http from 'k6/http'
-import * as helper from './helper.js'
+import * as helper from './helper-11.js';
 
 export const options = {
-    cloud: {
-        distribution: { 'amazon:ie:dublin': { loadZone: 'amazon:ie:dublin', percent: 100 } },
+  cloud: {
+    distribution: { 'amazon:de:frankfurt': { loadZone: 'amazon:de:frankfurt', percent: 100 } },
+  },
+  scenarios: {
+    Scenario_1: {
+      executor: 'ramping-vus',
+      gracefulStop: '30s',
+      stages: [
+        { target: 5, duration: '30s' },
+        { target: 5, duration: '30s' },
+        { target: 0, duration: '30s' },
+      ],
+      exec: 'reservedLogin',
     },
-    scenarios: {
-        Scenario_1: {
-            executor: 'ramping-vus',
-            stages: [
-                { target: 3, duration: '30s' },
-                { target: 3, duration: '30s' },
-                { target: 0, duration: '30s' },
-            ],
-            exec: 'reserved_login',
-        },
-    },
+  },
 }
 
-export function reserved_login() {
-    let getCustomerAccountLoginPageRequest = {
-        method: "GET",
-        url: "https://www.reserved.com/gb/en/customer/account/login/",
-        params: {
-            tags: { name: "RE - Get Customer Account Login Page" }
-        }
+export function reservedLogin() {
+  let getCustomerAccountLoginPageRequest = {
+    method: 'GET',
+    url: 'https://www.reserved.com/gb/en/customer/account/login/',
+    params: {
+      tags: {
+        name: 'RE - Get Customer Account Login Page',
+      },
+    },
+  }
+
+  let getVarnishAjaxNewIndexRequest = {
+    method: 'GET',
+    url: 'https://www.reserved.com/gb/en/varnish/ajax/newindex/?1726864558415',
+    params: {
+      tags: {
+        name: 'RE - Get Varnish New Indes',
+      },
+    },
+  }
+
+  let getVarnishAjaxIndexRequest = {
+    method: 'GET',
+    url: 'https://www.reserved.com/gb/en/varnish/ajax/index/?1726864558424',
+    params: {
+      tags: {
+        name: 'RE - Get Varnish Index',
+      },
+    },
+  }
+
+  let postPageInfoRequest = {
+    method: 'POST',
+    url: 'https://www.reserved.com/gb/en/varnish/ajax/index/?1726864558424',
+    params: {
+      tags: {
+        name: 'RE - Post Page Info',
+      },
+    },
+  }
+
+  function postCustomerLoginRequest(form_key) {
+    return {
+      method: 'POST',
+      url: 'https://www.reserved.com/gb/en/ajx/customer/login/referer/aHR0cHM6Ly93d3cucmVzZXJ2ZWQuY29tL2diL2VuLw,,/uenc/aHR0cHM6Ly93d3cucmVzZXJ2ZWQuY29tL2diL2VuLw,,/?lpp_new_login',
+      body: {
+        'login[username]': 'performancetests0001@wp.pl',
+        'login[password]': 'Qweasd12@',
+        'login[remember_me]': '1',
+        'form_key': form_key
+      },
+      params: {
+        headers: {
+          "content-type": "application/x-www-form-urlencoded; charset=UTF-8"
+        },
+        tags: {
+          name: 'RE - Post Customer Login',
+        },
+      }
+    };
+  }
+
+  let getMainPageRequest = {
+    method: 'GET',
+    url: 'https://www.reserved.com/gb/en/',
+    params: {
+      tags: {
+        name: 'RE - Get Main Page',
+      },
     }
+  }
 
-    let getVarnishAjaxNewIndexRequest = {
-        method: "GET",
-        url: "https://www.reserved.com/gb/en/varnish/ajax/newindex/?1729587785216",
-        params: {
-            tags: { name: "RE - Get Varnish New Index" }
-        }
-    }
+  group('Login Page - https://www.reserved.com/gb/en/customer/account/login/#login', function () {
+    let getCustomerAccountLoginPageResponse = http.get(getCustomerAccountLoginPageRequest.url, getCustomerAccountLoginPageRequest.params);
+    check(getCustomerAccountLoginPageResponse, { 'GET - Customer Account Login Page status was 200': (r) => r.status == 200 });
+    let formKey = helper.getFormKey(getCustomerAccountLoginPageResponse)
 
-    let getVarnishAjaxIndexRequest = {
-        method: "GET",
-        url: "https://www.reserved.com/gb/en/varnish/ajax/index/?1729587785216",
-        params: {
-            tags: { name: "RE - Get Varnish Index" }
-        }
-    }
+    let getVarnishAjaxNewIndexResponse = http.get(getVarnishAjaxNewIndexRequest.url, getVarnishAjaxNewIndexRequest.params)
+    check(getVarnishAjaxNewIndexResponse, { 'GET - Varnish New Index status was 200': (r) => r.status == 200 });
 
-    let postPageInfoRequest = {
-        method: "POST",
-        url: "https://www.reserved.com/proxydirectory/549238991762/pageInfo",
-        params: {
-            tags: {
-                name: "RE - Post Page Info"
-            }
-        }
-    }
+    let getVarnishAjaxIndexResponse = http.get(getVarnishAjaxIndexRequest.url, getVarnishAjaxIndexRequest.params)
+    check(getVarnishAjaxIndexResponse, { 'GET - Varnish Index status was 200': (r) => r.status == 200 });
 
-    function postCustomerLoginRequest(form_key) {
-        return {
-            method: 'POST',
-            url: 'https://www.reserved.com/gb/en/ajx/customer/login/referer/aHR0cHM6Ly93d3cucmVzZXJ2ZWQuY29tL2diL2VuLw,,/uenc/aHR0cHM6Ly93d3cucmVzZXJ2ZWQuY29tL2diL2VuLw,,/?lpp_new_login',
-            body: {
-                'login[username]': 'performancetests0001@wp.pl',
-                'login[password]': 'Qweasd12@',
-                'login[remember_me]': '1',
-                form_key: form_key,
-            },
-            params: {
-                headers: {
-                    'content-type': 'application/x-www-form-urlencoded; charset=UTF-8',
-                },
-                tags: {
-                    name: 'RE - Post Customer Login'
-                }
-            }
-        }
-    }
+    let postPageInfoResponse = http.post(postPageInfoRequest.url, null, postPageInfoRequest.params)
+    check(postPageInfoResponse, { 'POST - Page Info status was 200': (r) => r.status == 200 });
 
-    let getMainPageRequest = {
-        method: 'GET',
-        url: 'https://www.reserved.com/gb/en/',
-        params: {
-            tags: { name: "RE - Get Main Page" }
-        }
-    }
+    let postCustomerLoginResponse = http.post(postCustomerLoginRequest().url, postCustomerLoginRequest(formKey).body, postCustomerLoginRequest().params)
+    check(postCustomerLoginResponse, { 'POST - Customer Login status was 200': (r) => r.status == 200 });
+  })
 
-    group('Login Page - https://www.reserved.com/gb/en/customer/account/login/#login', function () {
-        let getCustomerAccountLoginPageResponse = http.get(getCustomerAccountLoginPageRequest.url, getCustomerAccountLoginPageRequest.params)
-        let formKey = helper.getFormKey(getCustomerAccountLoginPageResponse)
+  group('Main Page After User Log In - https://www.reserved.com/gb/en/', function () {
+    let getMainPageResponse = http.get(getMainPageRequest.url, getMainPageRequest.params);
+    check(getMainPageResponse, { 'GET - Main Page status was 200': (r) => r.status == 200 });
 
-        http.get(getVarnishAjaxNewIndexRequest.url, getVarnishAjaxNewIndexRequest.params)
+    let getVarnishAjaxNewIndexResponse = http.get(getVarnishAjaxNewIndexRequest.url, getVarnishAjaxNewIndexRequest.params)
+    check(getVarnishAjaxNewIndexResponse, {
+      'GET - Varnish New Index status was 200': (r) => r.status == 200,
+      'User has been logged in successfully': (r) => r.body.includes('Tomasz') == true
+    });
 
-        http.get(getVarnishAjaxIndexRequest.url, getVarnishAjaxIndexRequest.params)
+    let getVarnishAjaxIndexResponse = http.get(getVarnishAjaxIndexRequest.url, getVarnishAjaxIndexRequest.params)
+    check(getVarnishAjaxIndexResponse, {
+      'GET - Varnish Index status was 200': (r) => r.status == 200,
+      'User has been logged in successfully': (r) => r.body.includes('Tomasz') == true
+    });
 
-        http.post(postPageInfoRequest.url, null, postPageInfoRequest.params)
-
-        http.post(postCustomerLoginRequest().url, postCustomerLoginRequest(formKey).body, postCustomerLoginRequest().params)
-    })
-
-    group('Main Page after User Log in - https://www.reserved.com/gb/en/', function () {
-        http.get(getMainPageRequest.url, getMainPageRequest.params)
-
-        http.get(getVarnishAjaxNewIndexRequest.url, getVarnishAjaxNewIndexRequest.params)
-
-        http.get(getVarnishAjaxIndexRequest.url, getVarnishAjaxIndexRequest.params)
-
-        http.post(postPageInfoRequest.url, null, postPageInfoRequest.params)
-    })
+    let postPageInfoResponse = http.post(postPageInfoRequest.url, null, postPageInfoRequest.params)
+    check(postPageInfoResponse, { 'POST - Page Info status was 200': (r) => r.status == 200 });
+  })
 }
